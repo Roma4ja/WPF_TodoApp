706f49511a11484af58869181ab2b777d32c83b8
Roma4ja <ilichev_roma@list.ru>
2026-06-22 01:01:33 +0300

    Первый WPF проект — To-Do App

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows;
+
+namespace WPF_TodoApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void AddTask_Click(object sender, RoutedEventArgs e)
+        {
+            string task = TaskInput.Text.Trim();
+
+            if (string.IsNullOrEmpty(task))
+            {
+                MessageBox.Show("Введите задачу!");
+                return;
+            }
+
+            TaskList.Items.Add(task);
+            TaskInput.Clear();
+        }
+
+        
+        private void DeleteTask_Click(object sender, RoutedEventArgs e)
+        {
+            if (TaskList.SelectedItem == null)
+            {
+                MessageBox.Show("Выберите задачу для удаления!");
+                return;
+            }
+
+            TaskList.Items.Remove(TaskList.SelectedItem);
+        }
+    }
+}
